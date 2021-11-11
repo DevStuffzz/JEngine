400ef7d6e5b93e12d464ea413a507a88e8509b07
DevStuffz <67448356+DevStuffzz@users.noreply.github.com>
2021-11-11 17:29:27 -0500

    Collisions

---
+++ src/com/jengine/engine/ecs/Collision.java
@@ -0,0 +1,43 @@
+package com.jengine.engine.ecs;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jengine.engine.io.Input;
+
+public class Collision extends Component {
+
+	private List<Collision> otherColliders;
+
+	public Collision(Entity e, Input i) {
+		super(e, i);
+		otherColliders = new ArrayList<Collision>();
+	}
+
+	
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+	}
+
+
+	public boolean checkCollision(Collision other) {
+		Rectangle collider = new Rectangle((int)this.entity.location.x, (int)this.entity.location.y, this.entity.width, this.entity.height);
+		return collider.intersects(new Rectangle((int)other.entity.location.x, (int)other.entity.location.y, other.entity.width, other.entity.height));
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
